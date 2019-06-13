dabe078d00947830620388e81c50a01213396724
aaharbaugh <aaronmharbaugh@gmail.com>
2019-06-13 18:09:06 -0500

    was able to create several new roles. stil getting a feel for everything

---
+++ src/main/java/com/lambdaschool/zoo/services/ZooService.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.zoo.services;
+
+import com.lambdaschool.zoo.models.Zoo;
+import com.lambdaschool.zoo.views.CountAnimalsPerZoo;
+
+import java.util.ArrayList;
+
+public interface ZooService {
+
+    ArrayList<Zoo> findAll();
+
+    void delete(long id);
+
+    ArrayList<CountAnimalsPerZoo> getCountAnimalsPerZoo();
+
+}
